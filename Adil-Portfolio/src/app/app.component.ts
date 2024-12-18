1255db25cb135b339427c6293227c74a789320d7
adil.s <adil.s@theswipewire.in>
2024-12-18 12:45:16 +0530

    Angular insstalled

---
+++ Adil-Portfolio/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet,LandingpageComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'Adil-Portfolio';
+}
---
+++ Adil-Portfolio/src/app/landingpage/landingpage.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app.component';
+
+@Component({
+  selector: 'app-landingpage',
+  standalone: true,
+  imports: [AppComponent],
+  templateUrl: './landingpage.component.html',
+  styleUrl: './landingpage.component.scss'
+})
+export class LandingpageComponent {
+
+}
